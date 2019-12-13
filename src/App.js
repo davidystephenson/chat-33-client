09ea12eb41199083547069fd4589cc26145fabfa
David Y. Stephenson <david@davidystephenson.com>
2019-12-13 15:53:33 +0100

    Move action creation to server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* global EventSource */
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
   stream = new EventSource('http://localhost:4000/stream')
@@ -9,15 +10,37 @@ class App extends Component {
     this.stream.onmessage = event => {
       console.log('event.data test:', event.data)
 
-      const parsed = JSON.parse(event.data)
+      const action = JSON.parse(event.data)
+      console.log('action test:', action)
 
-      console.log('parsed test:', parsed)
+      this.props.dispatch(action)
     }
   }
 
   render () {
-    return <div>client</div>
+    console.log('this.props test:', this.props)
+
+    const { messages } = this.props
+
+    const list = messages
+      .map(message => <p key={message.id}>{message.text}</p>)
+
+    return <div>{list}</div>
   }
 }
 
-export default App
+// get data from store
+function mapStateToProps (state) {
+  // state is the current data in the redux store
+
+  // Each property of the object becomes a props of the component
+  return {
+    messages: state // Inside of the component, this.props.messages will be the entire state of the redux store
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
+// const anotherFunction = connect(mapStateToProps)
+// const connected = anotherFunction(App)
+// export default connected
